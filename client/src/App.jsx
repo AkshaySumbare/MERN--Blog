3ca8209af843d95d897883d91b4310dbae6c3c32
Akshay <sumbareakshay@gmail.com>
2024-02-09 18:23:16 +0530

    complete sidebar of the dsahboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,32 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import  Dashboard from './pages/Dashboard';
-import Projects from './pages/Projects';
-import Header from './Components/Header'
-import Footer from './Components/Footer';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import PrivateRoute from "./Components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header></Header>
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/about' element={<About />}/>
-      <Route path='/sign-in' element={<SignIn />} />
-      <Route path='/sign-up' element={<SignUp />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-      <Route path='/projects' element={<Projects />} />
-      
-    </Routes>
-    <Footer></Footer>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        
+        <Route element={<PrivateRoute/>}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+       
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <Footer/>
     </BrowserRouter>
-  )
+  );
 }
