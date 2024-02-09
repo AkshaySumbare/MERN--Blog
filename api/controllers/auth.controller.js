5878955340f04cb7c40c4bec795614eddcd29e53
Akshay <sumbareakshay@gmail.com>
2024-02-09 08:37:29 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/User.model.js";
-import bcrypt from "bcryptjs";
+import bcryptjs from "bcryptjs";
 import errorHandler from '../utils/error.js'
+import  Jwt  from "jsonwebtoken";
 
 
 export const signup = async (req, res, next) => {
@@ -17,7 +18,7 @@ export const signup = async (req, res, next) => {
     next(errorHandler(400, 'All fiels are required'));
   }
 
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({
     username: username,
     email: email,
@@ -30,3 +31,32 @@ export const signup = async (req, res, next) => {
    next(error);
   }
 };
+
+
+export const signin = async(req, res,next)=>{
+  const {email, password } = req.body;
+  if (
+    !email ||
+    !password ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400, 'All fiels are required'));
+  }
+  try {
+    const validUser = await User.findOne({email});
+    if (!validUser) {
+     return next(errorHandler(404, 'User not found'));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+     return next(errorHandler(400, 'Invalid password'));
+    }
+      const token = Jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+      const {password: pass, ...rest} = validUser._doc; //to hide the password 
+      res.status(200).cookie('access_token', token, {
+        httpOnly: true}).json(rest);
+  } catch (error) {
+   next(error);
+  }
+}
