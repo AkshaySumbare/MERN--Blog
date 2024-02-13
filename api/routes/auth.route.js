5965a2f6b498193a9dbeeaebee886520c0feaa83
Akshay <sumbareakshay@gmail.com>
2024-02-13 09:56:56 +0530

    complete post page functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import {signup, signin, google} from '../controllers/auth.controller.js'
 const router = express.Router();
- import cors from 'cors';
 
- router.use(cors());
+
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.post('/google', google)
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import PrivateRoute from "./Components/PrivateRoute";
 import OnlyPrivatePrivateRoute from "./Components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
 
 export default function App() {
   return (
@@ -33,6 +34,7 @@ export default function App() {
         </Route>
        
         <Route path="/projects" element={<Projects />} />
+        <Route path="/post/:postSlug" element={<PostPage />} />
       </Routes>
       <Footer/>
     </BrowserRouter>
