a763cdf086b49257a446962450de7327e05cc8d0
Akshay <sumbareakshay@gmail.com>
2024-02-15 12:14:37 +0530

    updated

--- client/src/Components/DashSidebar.jsx
+++ client/src/Components/DashSidebar.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Sidebar } from "flowbite-react";
-import { HiAnnotation, HiArrowSmRight, HiChartPie, HiDocumentText, HiOutlineUserGroup, HiUser } from "react-icons/hi";
+import {
+  HiAnnotation,
+  HiArrowSmRight,
+  HiChartPie,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiUser,
+} from "react-icons/hi";
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
-import {useSelector} from 'react-redux'
+import { useSelector } from "react-redux";
 
 export default function DashSidebar() {
   const location = useLocation();
-  const {currentUser} = useSelector(state => state.user)
+  const { currentUser } = useSelector((state) => state.user);
   const [tab, setTab] = useState("");
   const dispatch = useDispatch();
   useEffect(() => {
@@ -20,19 +27,16 @@ export default function DashSidebar() {
     }
   }, [location.search]);
 
-
-   
-  const handleSignOut = async () =>{
+  const handleSignOut = async () => {
     try {
-      const res = await fetch ('/api/user/signout',{
-        method: 'POST',
-      })
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
       const data = await res.json();
       if (!res.ok) {
-       console.log(data.message); 
-      }
-      else{
-         dispatch(signoutSuccess());
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
       }
     } catch (error) {
       console.log(error.message);
@@ -43,18 +47,22 @@ export default function DashSidebar() {
     <Sidebar className="w-full md:w-56 ">
       <Sidebar.Items>
         <Sidebar.ItemGroup className="flex flex-col gap-1">
-          {   
-            currentUser && currentUser.isAdmin && (
-              <Link to='/dashboard?tab=dash'>
-                <Sidebar.Item active={tab === 'dash' || !tab ===!tab } icon={HiChartPie} as='div'>Dashboard</Sidebar.Item>
-                </Link>
-            )
-          }
+          {currentUser && currentUser.isAdmin && (
+            <Link to="/dashboard?tab=dash">
+              <Sidebar.Item
+                active={tab === "dash" || !tab === !tab}
+                icon={HiChartPie}
+                as="div"
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === "profile"}
               icon={HiUser}
-              label={currentUser.isAdmin ? 'Admin' :'User'}
+              label={currentUser.isAdmin ? "Admin" : "User"}
               labelColor="dark"
               as="div"
             >
@@ -62,28 +70,44 @@ export default function DashSidebar() {
             </Sidebar.Item>
           </Link>
           {currentUser.isAdmin && (
-            <Link to='/dashboard?tab=posts'>
-            <Sidebar.Item active={tab === 'post'} icon={HiDocumentText} as='div'>
-              Posts
-            </Sidebar.Item>
-          </Link>
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item
+                active={tab === "post"}
+                icon={HiDocumentText}
+                as="div"
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
           )}
-           {currentUser.isAdmin && (
+          {currentUser.isAdmin && (
             <>
-            <Link to='/dashboard?tab=users'>
-            <Sidebar.Item active={tab === 'users'} icon={HiOutlineUserGroup} as='div'>
-              Users
-            </Sidebar.Item>
-          </Link>
-           <Link to='/dashboard?tab=comments'>
-            <Sidebar.Item active={tab === 'comments'} icon={HiAnnotation} as='div'>
-              Comments
-            </Sidebar.Item>
-          </Link>
-          </>
+              <Link to="/dashboard?tab=users">
+                <Sidebar.Item
+                  active={tab === "users"}
+                  icon={HiOutlineUserGroup}
+                  as="div"
+                >
+                  Users
+                </Sidebar.Item>
+              </Link>
+              <Link to="/dashboard?tab=comments">
+                <Sidebar.Item
+                  active={tab === "comments"}
+                  icon={HiAnnotation}
+                  as="div"
+                >
+                  Comments
+                </Sidebar.Item>
+              </Link>
+            </>
           )}
-          
-          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer"onClick={handleSignOut}>
+
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+            onClick={handleSignOut}
+          >
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
